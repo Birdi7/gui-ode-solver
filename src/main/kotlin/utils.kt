57d9214f7785e87d2091638289988c9bd2d48b63
birdi <egor.birdi7@gmail.com>
2019-11-05 00:38:04 +0300

    change initial numberOfSteps

--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -16,7 +16,7 @@ class InitialValuesInfo(
     x0: Double = 0.0,
     y0: Double = 1.0,
     xMax: Double = 20.0,
-    numberOfSteps: Int = 15
+    numberOfSteps: Int = 40
 ) {
     private val x0Property = SimpleDoubleProperty(x0)
     var x0 by x0Property
